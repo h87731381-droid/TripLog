8fd443f60c12e46e398431aa70703eb4e35f5f59
h87731381-droid <h87731381@gmail.com>
2026-04-20 14:51:21 +0900

    sdf

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import GeocoderMap  from "./comp/GeocoderMap";
+import attStyle from './pages/attrantions/attrantions.scss';
 
 export default function Home() {
   return (
-    <main>
+    <main className={attStyle}>
       <h1>Google Geocoder</h1>
       <GeocoderMap />
     </main>
