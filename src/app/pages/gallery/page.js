8e2e1914006c355b85d13e1816426e7e0f954453
hyewon <nahawon2ya@gmail.com>
2026-04-20 16:36:52 +0900

    dgfs

--- src/app/pages/gallery/page.js
+++ src/app/pages/gallery/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function page() {
+function gallery() {
   return (
-    <div>page</div>
+    <div>
+      <div>갤러리</div>
+      <div></div>
+    </div>
   )
 }
 
-export default page
+export default gallery
